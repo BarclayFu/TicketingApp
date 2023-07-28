15556b040063369c1abba5a5fb3c22c6c17bbdda
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-07-28 19:11:42 -0400

    ingress-Nginx setup
    When encountering a Chrome Security warning, you just go to go over to this tab, click anywhere inside of it, and then type out thisisunsafe .

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,11 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
-app.listen(3000,() =>{
+app.get('/api/users/currentuser', (req, res)=>{
+  res.send('Hi!');
+});
+
+app.listen(3000,() => {
   console.log('Listening on port 3000!');
   console.log('HI!');
 });
