05cd6a37b0ca1a8da6ab26629a3d7f1848f60e20
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-07-31 22:51:42 -0400

    adding validation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,9 +4,15 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res)=>{
-  res.send('Hi!');
-});
+import { currentuserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000,() => {
   console.log('Listening on port 3000!');
