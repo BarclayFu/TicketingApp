826e159309a67d7001f2d16769cd8391efdad041
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-08-02 19:37:30 -0400

    Async Error handing
    use this package 'express-async-errors'

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { json } from "body-parser";
+import 'express-async-errors';
 
 import { currentuserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./ errors/not-found-err";
 
 const app = express();
 app.use(json());
@@ -15,7 +17,13 @@ app.use(currentuserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async(req, res)=>{
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
+
 
 app.listen(3000,() => {
   console.log('Listening on port 3000!');
