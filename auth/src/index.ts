1134c38a9ca231434fb74052c56e623e43c54b89
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-08-03 11:00:05 -0400

    connecting to mongoDB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { json } from "body-parser";
 import 'express-async-errors';
+import mongoose from "mongoose";
 
 import { currentuserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -24,8 +25,19 @@ app.all('*', async(req, res)=>{
 
 app.use(errorHandler);
 
+const start =async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
 
-app.listen(3000,() => {
-  console.log('Listening on port 3000!');
-  console.log('HI!');
-});
+  app.listen(3000,() => {
+    console.log('Listening on port 3000!');
+    console.log('HI!');
+  });
+  
+};
+
+start();
