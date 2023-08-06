fe7c18b5a2388c6fea9f066b2e7da5e85f8754ed
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-08-06 13:26:01 -0400

    augmenting type definitions

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,11 @@
 import express from 'express';
 import jwt  from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req,res) =>{
-  // You will see an error here if there is no "?"
-  // because we have to check req do exist ( !req.seesion || !req.sesson.jwt)
-  // using "?" equals to !req.session
-  if(!req.session?.jwt){
-    return res.send({currentUser: null});
-  }
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({currentUser: payload});
-  } catch (error) {
-    res.send({currentUser: null});
-  }
+router.get('/api/users/currentuser', currentUser,(req,res) =>{
+  res.send({currentUser:req.currentUser  || null});
   
 });
 
