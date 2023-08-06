734018e2cbc974dd8d597175e48f20982f307314
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-08-06 08:54:17 -0400

    apply sign in logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
+import { User } from '../models/user';
+import { Password } from '../services/password';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../ errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -14,8 +18,34 @@ body('password')
   .withMessage('You must supply a password')
 ],
 validateRequest,
-(req: Request,res: Response) =>{
+async (req: Request,res: Response) =>{
+  const {email, password} = req.body;
 
+  const existingUser = await User.findOne({email});
+  if(!existingUser){
+    throw new BadRequestError('Invalid credentials');
+  }
+
+  const passwordsMatch = await Password.compare(existingUser.password, password);
+  if(!passwordsMatch){
+    throw new BadRequestError('Invalid credentials');
+  }
+
+  // Genertate JWT
+
+  const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email
+  }, process.env.JWT_KEY!); // "!" tells Typrscript that we have checked the variable  
+                            // otherwise there will be a  wavy line!
+  
+
+  //Store it on session object
+  req.session = {
+    jwt: userJwt
+  };
+
+  res.status(200).send(existingUser);  
 });
 
 export {router as signinRouter}; 
