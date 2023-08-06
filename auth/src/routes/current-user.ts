d4faca62cc7cabc0936b9bdd9f7d48ed41bb0caf
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-08-06 10:22:21 -0400

    build current user handler and return the current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt  from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req,res) =>{
-  res.send('Hi there!');
+  // You will see an error here if there is no "?"
+  // because we have to check req do exist ( !req.seesion || !req.sesson.jwt)
+  // using "?" equals to !req.session
+  if(!req.session?.jwt){
+    return res.send({currentUser: null});
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({currentUser: payload});
+  } catch (error) {
+    res.send({currentUser: null});
+  }
+  
 });
 
 export {router as currentuserRouter}; 
