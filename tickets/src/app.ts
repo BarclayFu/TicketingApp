2df901ed650bb85c0e22769715a41bac8cd8fca0
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-08-18 22:52:03 -0400

    better error logging and complete index route implementation

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,8 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@bsftickets/common/build';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +20,8 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 
 app.all('*', async (req, res) => {
