b5e16e368b8a57bcc1192f8586a7072630d67ec5
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-08-30 00:09:51 -0400

    Order Model setup

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,10 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@bsftickets/common/build';
-import { deleteOrderRouter } from '../routes/delete';
-import { indexOrderRouter } from '../routes';
-import { newOrderRouter } from '../routes/new';
-import { showOrderRouter } from '../routes/show';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
