f4dde65cd35f8bbded5d8817520a03ecccf594fb
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-09-03 23:10:57 -0400

    Order expiration time

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -7,6 +7,8 @@ import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post('/api/orders',[
   body('ticketId')
     .not()
@@ -21,33 +23,31 @@ router.post('/api/orders',[
     throw new NotFoundError();
   }
 
+  const isReserved = await ticket.isReserved();
+  if(isReserved){
+    throw new BadRequestError('Ticket is already reserved');
+  }
+
   // Make sure that this ticket is not already reserved
   // Run query to look at all orders. Find an order where the ticket
   // is the ticket we just found and the orders status is not cancelled
   // If we find an order from that means the ticket is reserved
 
-  const existingOrder = await Order.findOne({
-    ticket: ticket,
-    status:{
-      $in: [
-        OrderStatus.Created,
-        OrderStatus.AwaitingPayment,
-        OrderStatus.Complete
-      ],
-    },
-  });
-  if(existingOrder){
-    throw new BadRequestError('Ticket is already reserved');
-  }
-
   
   // Calculate an expiration date for this order
-
+  const expiration = new Date();
+  expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
   // Build the order and save it to the database
-
+  const order = Order.build({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expireAt: expiration,
+    ticket
+  });
+  await order.save();
   // Publish an event saying that an order was created
 
-  res.send({});
+  res.status(201).send(order);
 });
 
 export {router as newOrderRouter};
