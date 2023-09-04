36ab92949b2509f53842fc34676c0796d7ec4866
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-09-03 23:44:27 -0400

    update package to latest version after updating common packages

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,11 @@
+it('returns an error if the ticket does not exist', async ()=>{
+
+});
+
+it('returns an error if thr ticket is already reserved', async ()=>{
+
+});
+
+it('reserves a ticket' , async ()=>{
+
+});
