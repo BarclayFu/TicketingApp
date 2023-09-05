df10fa6661f16ebd5b673f3b20e5e77f4d49d86e
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-09-04 21:17:25 -0400

    asserting tickets exist

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,5 +1,17 @@
-it('returns an error if the ticket does not exist', async ()=>{
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../app";
 
+it('returns an error if the ticket does not exist', async ()=>{
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId
+    })
+    .expect(404);
 });
 
 it('returns an error if thr ticket is already reserved', async ()=>{
