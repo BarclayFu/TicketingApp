40fbc21b365f4b9b8733e17c09fbc21f4c358923
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-09-10 16:02:23 -0400

    testing listener and ack call

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,17 @@
+const setup = async () =>{
+  // Create a listener
+
+  // Create and save a ticket
+
+  // Create a fake data object
+
+  // return all of this stuff
+};
+
+it('finds, updatas and save a ticket', async() =>{
+
+});
+
+it('acks the message', async () =>{
+  
+});
