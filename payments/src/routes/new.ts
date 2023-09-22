02cbee499efc7c90b2fddf1f34d7196bc72c77b3
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-09-22 17:30:25 -0400

    validating order payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest, BadRequestError, NotFoundError } from '@bsftickets/common/build';
+import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus } from '@bsftickets/common/build';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -15,6 +15,21 @@ body('orderId')
   .isEmpty()],
 validateRequest,
   async(req:Request, res: Response) =>{
+    const {token, orderId} = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if(!order){
+      throw new NotFoundError();
+    }
+
+    if(order.userId !== req.currentUser!.id){
+      throw new NotAuthorizedError();
+    }
+
+    if(order.status === OrderStatus.Cancelled){
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
     res.send({success: true});
   }
 );
