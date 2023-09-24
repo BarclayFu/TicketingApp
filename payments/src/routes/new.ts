6acf6f5f00bf2ab677e53a4838b0a0fe1c3bd8aa
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-09-23 23:09:51 -0400

    create a charge with stripe

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus } from '@bsftickets/common/build';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -30,6 +31,12 @@ validateRequest,
     if(order.status === OrderStatus.Cancelled){
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
+
+    await stripe.charges.create({
+      currency:'usd',
+      amount: order.price * 100,
+      source: token
+    });
     res.send({success: true});
   }
 );
