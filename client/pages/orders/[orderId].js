2fdad03eb56c9436e74866bfd72779ef3bc148fc
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-10-03 15:23:04 -0400

    pay for an order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,17 @@
 import {useEffect, useState} from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({order, currentUser}) =>{
   const [timeLeft, setTimeLeft] = useState(0);
+  const {doRequest, errors} = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body:{
+      orderId: order.id
+    },
+    onSuccess:(payment) => console.log(payment)
+  });
 
   useEffect(() => {
     const findTimeLeft = () =>{
@@ -23,11 +32,12 @@ const OrderShow = ({order, currentUser}) =>{
 
   
   return <div>Time left to pay : {timeLeft} seconds
-    <StripeCheckout token={(token => console.log(token))}
+    <StripeCheckout token={({id}) => doRequest({token : id})}
     stripeKey='pk_test_51Nti5BKMzBqWMkbXDWUQyNA81UpVPbqtVqA0okfkjG7esoVlBFD8xsoxQx1JM11Si3QrkckzyiUBSwrfe7AGguvt00twLfcVuc'
     amount={order.ticket.price * 100}
     email={currentUser.email}
     />
+    {errors}
   </div>;
 };
 
