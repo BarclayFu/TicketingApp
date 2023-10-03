75ae8cbe67e4b0511afa83d548adb5979b4b5a21
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-10-03 15:00:35 -0400

    setup stripe configurations

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
-const OrderShow = ({order}) =>{
+const OrderShow = ({order, currentUser}) =>{
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
@@ -21,7 +22,13 @@ const OrderShow = ({order}) =>{
   }
 
   
-  return <div>Time left to pay : {timeLeft} seconds</div>
+  return <div>Time left to pay : {timeLeft} seconds
+    <StripeCheckout token={(token => console.log(token))}
+    stripeKey='pk_test_51Nti5BKMzBqWMkbXDWUQyNA81UpVPbqtVqA0okfkjG7esoVlBFD8xsoxQx1JM11Si3QrkckzyiUBSwrfe7AGguvt00twLfcVuc'
+    amount={order.ticket.price * 100}
+    email={currentUser.email}
+    />
+  </div>;
 };
 
 OrderShow.getInitialProps = async(context, client) =>{
